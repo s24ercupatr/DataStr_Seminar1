6feae79385674172426593d9142f10d9407473af
s24ercupatr <s24ercupatr@venta.lv>
2026-03-05 09:21:12 +0200

    first commit

---
+++ src/datastr/MyArrayList.java
@@ -0,0 +1,40 @@
+package datastr;
+
+public class MyArrayList {
+	private char[] list;
+	private int howManyElements = 0;
+	private final int DEFAULT_SIZE = 8;
+	private int size = DEFAULT_SIZE;
+	
+	public MyArrayList() {
+		list = new char[size];
+	}
+	
+	public MyArrayList(int inputSize) {
+		if(inputSize > 0 && inputSize <10000) {
+			size = inputSize;
+		}
+		list = new char[size];
+	}
+
+	private boolean isEmpty() {
+		return (howManyElements == 0);
+	}
+	
+	private boolean isFull() {
+		return (howManyElements == size);
+	}
+	
+	private void resize() {
+		int newSize = (howManyElements < 200) ? (size * 2) : (int)(size * 1.5);
+		char[] newList = new char[newSize];
+
+		for(int i = 0; i < howManyElements; i++) {
+			newList[i] = list[i];
+		}
+		
+		list = newList;
+		size = newSize;
+		System.gc();
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module DataStr_Seminar1 {
+}
